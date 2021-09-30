b1df6e7e4ff72102f95673357bc0274802b2a3ba
Enade P. Istyastono <enade.istyastono@gmail.com>
2021-09-30 15:07:48 +0700

    Update pdb2plif.sh

--- pdb2plif.sh
+++ pdb2plif.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ -z  "$1" ];
+if [ -z  "$2" ];
 then
-echo "Ligand file name as the 1st variable was not provided"
-echo "Ligand file name could be obtained by pilot running SPORES --mode splitpdb"
+echo "Neither protein-ligand complex pdb file name nor ligand file name was provided"
+echo "Ligand file name could be obtained by pilot running SPORES --mode splitpdb on the pdb file of the protein-ligand complex"
 echo "Usage: ./pdb2plif.sh [pdb file name] [ligand file name]"
 echo "Example: ./pdb2plif.sh 4H3X.pdb ligand_10B306_0.mol2"
 else
@@ -15,7 +15,7 @@ hippos=~/.hippos/hippos.py
 
 # Normally no change required below this point
 $SPORES --mode splitpdb $1
-cp $2 ligand.mol2
+$SPORES --mode complete $2 ligand.mol2
 $PLANTS --mode bind ligand.mol2 5 protein.mol2
 echo "scoring_function chemplp" > plantsconfig
 echo "search_speed speed4" >> plantsconfig
@@ -23,11 +23,10 @@ echo "protein_file protein.mol2" >> plantsconfig
 echo "ligand_file ligand.mol2" >> plantsconfig
 echo "output_dir results" >> plantsconfig
 echo "write_multi_mol2 0" >> plantsconfig
+echo "rescore_mode no_simplex" >> plantsconfig
+echo "write_rescored_structures 1" >> plantsconfig
 cat bindingsite.def >> plantsconfig
 $PLANTS --mode rescore plantsconfig
-tricky="`head -2 ligand.mol2 | tail -1`_entry_00001_conf_01"
-cp ligand.mol2 results/$tricky.mol2
-touch results/$tricky\_protein.mol2
 echo "docking_method plants" > config.hippos
 echo "docking_conf plantsconfig" >> config.hippos
 echo "output_mode full full_nobb" >> config.hippos
@@ -51,4 +50,12 @@ for j in $(seq 1 `cat .tmp.res.lst.type | wc -l`); do awk -v j="$j" '{print subs
 paste -d, .tmp.res.lst.type .tmp.nobb.human | grep ",1" | sed "s/,1//g" > nobb.plif-h.txt
 paste -d, .tmp.res.lst.type .tmp.full.human | grep ",1" | sed "s/,1//g" > full.plif-h.txt
 rm .tmp.full.human .tmp.nobb.human .tmp.plif.txt .tmp.res.lst .tmp.res.lst.type
+echo " "
+echo "The following are identifited interactions when the backbone atoms are taken into account:"
+cat full.plif-h.txt
+echo " "
+echo "The following are identifited interactions when the backbone atoms are not taken into account:"
+cat nobb.plif-h.txt
+echo " "
+echo "The interaction bitstrings were stored in plif_full.txt (with backbone atoms) and plif_nobb.txt (without backbone atoms)"
 fi
